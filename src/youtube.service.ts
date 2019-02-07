7d886092087c5ca342d89eb31cc9e58f7af9d3bc
Daniel Antonio Conte <danconte72@gmail.com>
2019-02-07 14:40:19 -0200

    fix: add returning types

--- src/youtube.service.ts
+++ src/youtube.service.ts
@@ -9,11 +9,9 @@ export class YoutubeService {
   constructor(
     public http: HttpClient,
     @Inject(YoutubeConfigService) private config: YoutubeConfig
-  ) {
-    console.log('Hello YoutubeService');
-  }
+  ) {}
 
-  public async fetchPlaylists(channelId: string) {
+  public async fetchPlaylists(channelId: string): Promise<Array<Object>> {
     const url = `${this.config.apiURL}/playlists?part=snippet%2CcontentDetails&channelId=${channelId}&key=${this.config.apiKey}`;
     return this.http.get(url).toPromise()
     .then(
@@ -25,10 +23,10 @@ export class YoutubeService {
     );
   }
 
-  public async fetchVideos(channelId: string, playlist: string) {
+  public async fetchVideos(channelId: string, playlistId: string = ''): Promise<Array<Object>> {
     let url = `${this.config.apiURL}/search?key=${this.config.apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=20`;
-    if (playlist !== '') {
-      url = `${this.config.apiURL}/playlistItems?key=${this.config.apiKey}&playlistId=${playlist}&part=snippet,id&order=date&maxResults=20`;
+    if (playlistId !== '') {
+      url = `${this.config.apiURL}/playlistItems?key=${this.config.apiKey}&playlistId=${playlistId}&part=snippet,id&order=date&maxResults=20`;
     }
     return this.http.get(url).toPromise()
     .then(
